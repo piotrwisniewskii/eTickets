ee262f3e5dd43b62829ba2b385261093c5b09f1b
Piotr Wisniewski <piowisni@gmail.com>
2022-12-12 17:30:44 +0100

    added views

--- eTickets/Models/Producer.cs
+++ eTickets/Models/Producer.cs
@@ -7,8 +7,11 @@ namespace eTickets.Models
     {
         [Key]
         public int Id { get; set; }
+        [Display(Name = "Profile Picture")]
         public string ProfilePictureURL { get; set; }
+        [Display(Name = "FullName Picture")]
         public string FullName { get; set; }
+        [Display(Name = "Bio")]
         public string Bio { get; set; }
 
         // Relationships

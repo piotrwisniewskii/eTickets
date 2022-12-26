c620bb898249aeb3ae5039825d606d4a29e3ae68
Piotr Wisniewski <piowisni@gmail.com>
2022-12-26 10:35:43 +0100

    added Cinemas controller

--- eTickets/Models/Cinema.cs
+++ eTickets/Models/Cinema.cs
@@ -1,16 +1,22 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using eTickets.Data.Base;
+using System.ComponentModel.DataAnnotations;
 
 namespace eTickets.Models
 {
-    public class Cinema
+    public class Cinema : IEntityBase
     {
         [Key]
         public int Id { get; set; }
         [Display(Name= "Logo")]
+        [Required(ErrorMessage = "Cinema logo is required")]
+
         public string Logo { get; set; }
         [Display(Name= "Cinema Name")]
+        [Required(ErrorMessage = "Cinema Name is required")]
+
         public string Name { get; set; }
         [Display(Name = "Description")]
+        [Required(ErrorMessage = "Cinema Description is required")]
         public string Description { get; set; }
 
     }

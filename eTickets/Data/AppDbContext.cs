c77fff3e625bfeacf7875b7e6b985a04f30497e3
Piotr Wisniewski <piowisni@gmail.com>
2022-12-28 11:25:14 +0100

    added login

--- eTickets/Data/AppDbContext.cs
+++ eTickets/Data/AppDbContext.cs
@@ -1,13 +1,10 @@
 ﻿using eTickets.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace eTickets.Data
 {
-    public class AppDbContext : DbContext
+    public class AppDbContext : IdentityDbContext<ApplicationUser>
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
---
+++ eTickets/Data/ViewModels/LoginVM.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace eTickets.Data.ViewModels
+{
+    public class LoginVM
+    {
+        [Display(Name = "Email address")]
+        [Required(ErrorMessage ="Email address is required")]
+        public string EmaillAddress { get; set; }
+
+        [Display(Name = "Password")]
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+    }
+}

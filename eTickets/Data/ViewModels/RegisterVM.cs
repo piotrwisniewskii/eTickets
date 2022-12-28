79244406dfb64c6a0412d6298aa305bb2bc3b2d8
Piotr Wisniewski <piowisni@gmail.com>
2022-12-28 11:46:53 +0100

    added registration

---
+++ eTickets/Data/ViewModels/RegisterVM.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace eTickets.Data.ViewModels
+{
+    public class RegisterVM
+    {
+        [Display(Name = "Full Name")]
+        [Required(ErrorMessage = "Full Name is required")]
+        public string FullName { get; set; }
+
+        [Display(Name = "Email address")]
+        [Required(ErrorMessage ="Email address is required")]
+        public string EmaillAddress { get; set; }
+
+        [Display(Name = "Password")]
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+
+        [Display(Name ="Confirm password")]
+        [Required(ErrorMessage ="Confirm password is required")]
+        [DataType(DataType.Password)]
+        [Compare("Password",ErrorMessage ="Password do not match")]
+        public string ConfirmPassword { get; set; }
+
+    }
+}

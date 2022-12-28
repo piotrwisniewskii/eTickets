aff86e4dd3da37a33c92c77925a7818caa103368
Piotr Wisniewski <piowisni@gmail.com>
2022-12-28 12:15:17 +0100

    added identityPartialView

--- eTickets/Controllers/AccountController.cs
+++ eTickets/Controllers/AccountController.cs
@@ -75,5 +75,12 @@ namespace eTickets.Controllers
 
             return View("RegisterCompleted");
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Movies");
+        }
     }
 }
